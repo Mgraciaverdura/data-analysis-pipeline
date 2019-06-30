03810ce0c700b24bfc125a49690e3ba44b8c292f
Marta Gracia <mgraciaverdura@gmail.com>
2019-06-30 18:18:27 +0200

    Primer intento para generar el pdf

--- fpdf_crash.py
+++ fpdf_crash.py
@@ -1,6 +1,6 @@
 from fpdf import FPDF      # para crear pdf
 
-titulo='Plane crashes by location and by operator'
+titulo='Accidentes de avión por operadoras y por localizaciones'
 
 class PDF(FPDF):
     def header(self):  # cabecera
@@ -15,5 +15,39 @@ class PDF(FPDF):
         self.cell(ancho, 9, titulo, 1, 1, 'C', 1)  # titulo
         self.ln(10) # salto de linea
 
+    def footer(self):                                                # pie de pagina
+        self.set_y(-15)                                              # posicion a 1.5 cm desde abajo
+        self.set_font('Arial', 'I', 8)                               # fuente Arial italica 8
+        self.set_text_color(128)                                     # color texto en gray
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')   # numero de pagina
 
-        
+    def chapter_title(self, numero, etiqueta):                                 # titulo del capitulo
+        self.set_font('Arial', '', 12)                                         # fuente Arial 12
+        self.set_fill_color(200, 220, 255)                                     # color del fondo
+        self.cell(0, 6, 'Chapter %d : %s' % (numero, etiqueta), 0, 1, 'L', 1)  # titulo
+        self.ln(4)                                                             # salto de linea
+
+    def chapter_body(self, nombre, numero):                      # cuerpo del capitulo
+        with open(nombre, 'rb') as f:                            # se lee el archivo de texto
+            txt=f.read().decode('latin-1')
+        self.set_font('Times', '', 12)                           # fuente Times 12
+        self.multi_cell(0, 5, txt)                               # texto con saltos de linea (multicelda)
+        self.ln()                                                # salto de linea
+        self.set_font('', 'I')                                   # alusion en fuente italica
+        self.cell(0, 5, '(fin del capitulo {})'.format(numero))
+
+    def print_chapter(self, numero, titulo, nombre):    # imprime el capitulo
+        self.add_page()   # añade pagina
+        self.chapter_title(numero, titulo)    # numero y titulo de capitulo
+        self.chapter_body(nombre, numero)     # cuerpo del capitulo
+
+
+
+pdf=PDF() # inicia clase PDF
+pdf.set_title('Accidentes de avión por operadoras y por localizaciones') # titulo
+pdf.set_author('Marta Gracia')  # autor
+pdf.print_chapter(1,'Datos extraídos sobre accidentes de avión por operadoras y por localizaciones entre los años 1908 y 2019', 'main.txt') # capitulo único
+pdf.output('crash.pdf', 'F')  # guarda pdf
+
+
+
